3608822c12d79fa5c3df69550fe390380fce204f
Andrei <andry-93@bk.ru>
2019-07-08 23:59:34 +0300

    tab filter

--- sources/views/activities/index.js
+++ sources/views/activities/index.js
@@ -5,6 +5,47 @@ import {activitytypes} from "../../models/activitytypes";
 import SaveForm from "./savePopup";
 import "./style.css";
 
+function activitiesFilter(obj) {
+	const currentDate = new Date().setHours(0, 0, 0, 0);
+	let tomorrowDate = new Date(currentDate);
+	tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+	let lastDayWeek = new Date(currentDate);
+	lastDayWeek.setDate(lastDayWeek.getDate() - lastDayWeek.getDay() + 6);
+	obj.table.registerFilter(
+		obj.tabs,
+		{
+			compare: (val, filter, item) => {
+				const actualDate = new Date(item.DueDate).setHours(0, 0, 0, 0);
+				switch (filter) {
+					case "all":
+						return true;
+					case "today":
+						return +actualDate === +currentDate;
+					case "tomorrow":
+						return +actualDate === +tomorrowDate;
+					case "thisWeek":
+						return +actualDate >= +currentDate && +actualDate <= +lastDayWeek;
+					case "thisMonth":
+						return false;
+					case "overdue":
+						return +actualDate < +currentDate && item.State !== "Close";
+					case "completed":
+						return item.State === "Close";
+					default:
+						break;
+				}
+				return false;
+			}
+		},
+		{
+			getValue: filter => filter.getValue(),
+			setValue: (filter, value) => {
+				filter.setValue(value);
+			}
+		}
+	);
+}
+
 export default class Activities extends JetView {
 	config() {
 		let Toolbar = {
@@ -12,7 +53,25 @@ export default class Activities extends JetView {
 			borderless: true,
 			paddingX: 10,
 			elements: [
-				{},
+				{
+					view: "tabbar",
+					localId: "activitiesTab",
+					borderless: true,
+					options: [
+						{id: "all", value: "All"},
+						{id: "today", value: "Today"},
+						{id: "tomorrow", value: "Tomorrow"},
+						{id: "thisWeek", value: "This week"},
+						{id: "thisMonth", value: "This month"},
+						{id: "overdue", value: "Overdue"},
+						{id: "completed", value: "Completed"}
+					],
+					on: {
+						onChange: () => {
+							this.$$("activitiesTable").filterByAll();
+						}
+					}
+				},
 				{
 					view: "button",
 					css: "webix_primary",
@@ -63,7 +122,10 @@ export default class Activities extends JetView {
 			activitytypes.waitData
 		]).then(() => {
 			activities.filter();
+			const dataTable = this.$$("activitiesTable");
+			const tabFilter = this.$$("activitiesTab");
 			this.$$("activitiesTable").sync(activities);
+			activitiesFilter({table: dataTable, tabs: tabFilter});
 		});
 	}
 
