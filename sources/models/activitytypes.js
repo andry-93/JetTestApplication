b656c2cc52ce0d8ba350c53ec56f558440bc507d
azayats <azayats@xbsoftware.by>
2019-07-09 15:07:03 +0300

    delete type

--- sources/models/activitytypes.js
+++
@@ -1,9 +0,0 @@
-export const activityTypes = new webix.DataCollection({
-	url: "http://localhost:8096/api/v1/activitytypes/",
-	save: "rest->http://localhost:8096/api/v1/activitytypes/",
-	scheme: {
-		$init: (obj) => {
-			obj.value = obj.Value;
-		}
-	}
-});
